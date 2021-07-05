2506db5380a399bd301e3786e42a099ebd8449f6
Ish Kapoor <60035881+ishkapoor2000@users.noreply.github.com>
2021-07-05 20:22:58 +0530

    Update Code.ino

--- Code.ino
+++ Code.ino
@@ -1,3 +1,4 @@
+/* Declaring the variables and pins for 7 Segment display */
 int f = 13;
 int g = 12;
 int e = 11;
@@ -13,27 +14,28 @@ const int startButtonPin = 4;
 const int BuzzerPin = 3;
 
 int n[16][7] = {
-     /*a, b, c, d, e, f, g*/
-      {1, 1, 1, 1, 1, 1, 0}, /*0*/
-      {0, 1, 1, 0, 0, 0, 0}, /*1*/
-      {1, 1, 0, 1, 1, 0, 1}, /*2*/
-      {1, 1, 1, 1, 0, 0, 1}, /*3*/
-      {0, 1, 1, 0, 0, 1, 1}, /*4*/
-      {1, 0, 1, 1, 0, 1, 1}, /*5*/
-      {1, 0, 1, 1, 1, 1, 1}, /*6*/
-      {1, 1, 1, 0, 0, 0, 0}, /*7*/
-      {1, 1, 1, 1, 1, 1, 1}, /*8*/
-      {1, 1, 1, 1, 0, 1, 1}, /*9*/
-      {1, 1, 1, 0, 1, 1, 1}, /*A*/
-      {0, 0, 1, 1, 1, 1, 1}, /*b*/
-      {1, 0, 0, 1, 1, 1, 0}, /*C*/
-      {0, 1, 1, 1, 1, 0, 1}, /*d*/
-      {1, 0, 0, 1, 1, 1, 1}, /*E*/
-      {1, 0, 0, 0, 1, 1, 1}  /*F*/
-	};
+  /*a, b, c, d, e, f, g*/
+  {1, 1, 1, 1, 1, 1, 0}, /*0*/
+  {0, 1, 1, 0, 0, 0, 0}, /*1*/
+  {1, 1, 0, 1, 1, 0, 1}, /*2*/
+  {1, 1, 1, 1, 0, 0, 1}, /*3*/
+  {0, 1, 1, 0, 0, 1, 1}, /*4*/
+  {1, 0, 1, 1, 0, 1, 1}, /*5*/
+  {1, 0, 1, 1, 1, 1, 1}, /*6*/
+  {1, 1, 1, 0, 0, 0, 0}, /*7*/
+  {1, 1, 1, 1, 1, 1, 1}, /*8*/
+  {1, 1, 1, 1, 0, 1, 1}, /*9*/
+  {1, 1, 1, 0, 1, 1, 1}, /*A*/
+  {0, 0, 1, 1, 1, 1, 1}, /*b*/
+  {1, 0, 0, 1, 1, 1, 0}, /*C*/
+  {0, 1, 1, 1, 1, 0, 1}, /*d*/
+  {1, 0, 0, 1, 1, 1, 1}, /*E*/
+  {1, 0, 0, 0, 1, 1, 1}  /*F*/
+};
 
 void setup()
 {
+  /* Declaring inp/op of pins in setup */
   pinMode(f, OUTPUT);
   pinMode(g, OUTPUT);
   pinMode(e, OUTPUT);
@@ -50,50 +52,66 @@ void setup()
 
 void loop()
 {
-    int timerButtonState;
-    timerButtonState = digitalRead(timerButtonPin);
-    int startTimerButtonState;
-    startTimerButtonState = digitalRead(startButtonPin);
+  int timerButtonState;
+  timerButtonState = digitalRead(timerButtonPin);
+  /* 'timerButtonState' stores the current state [On/Off]
+     of the Timer-button */
+  int startTimerButtonState;
+  startTimerButtonState = digitalRead(startButtonPin);
+  /* 'startTimerButtonState' stores the current state
+     [On/Off] of the Start-button */
 
-    if (timerButtonState == LOW)
-    {
-        counter++;
-        if (counter >= 10) {
-            counter = 0;
-        }
-        Serial.print("Counter-Set: ");
-        Serial.println(counter);
-        num(counter);
-        delay(150);
+  if (timerButtonState == LOW)
+  {
+    /* When Timer-button is pressed,
+       thec counter increments and till it is <= 10. */
+    counter++;
+    if (counter >= 10) {
+      counter = 0;
     }
+    Serial.print("Counter-Set: ");
+    Serial.println(counter);
+    num(counter);
+    /* Displaying the value of counter on Display */
+    delay(150);
+  }
 
   if (startTimerButtonState == LOW) {
-  		startReverseTimer(counter);
+    /* On Start-button pressed,
+         the counter decrements and value is displayed on
+         7 Segment Display. */
+    startReverseTimer(counter);
   }
 }
 
 void startReverseTimer(int time) {
-	for (int i = 0; i < time; i++)
-    {
-        num(time - i);
-      	Serial.print("Counter-Reverse: ");
-        Serial.println(time - i);
-        delay(1000);
-  	}
-  	Serial.print("Counter-Reverse: Reset to ");
-  	num(0);
-  	tone(BuzzerPin,200);
-  	delay(1000);
-  	noTone(BuzzerPin);
+  /* Decresing the counter value and displaying it on 7 Segment Display */
+  for (int i = 0; i < time; i++)
+  {
+    num(time - i);
+    digitalWrite(dec, HIGH);
+    Serial.print("Counter-Reverse: ");
+    Serial.println(time - i);
+    delay(1000);
+  }
+  Serial.print("Counter-Reverse: Reset to ");
+  num(0); /* Setting the value back to Zero when the timer ends. */
+  /* digitalWrite(BuzzerPin, HIGH);
+     delay(1000);
+     digitalWrite(BuzzerPin, LOW); */
+  tone(BuzzerPin, 200); /* Alarming the buzzer when timer ends. */
+  delay(1000);
+  noTone(BuzzerPin);
 }
 
 void num(int i)
 {
-    digitalWrite(a, n[i][0]);
-    digitalWrite(b, n[i][1]);
-    digitalWrite(c, n[i][2]);
-    digitalWrite(d, n[i][3]);
-    digitalWrite(e, n[i][4]);
-    digitalWrite(f, n[i][5]);
-    digitalWrite(g, n[i][6]);
+  /* Writes the value as per the list for every LED in 7 Segment Display */
+  digitalWrite(a, n[i][0]);
+  digitalWrite(b, n[i][1]);
+  digitalWrite(c, n[i][2]);
+  digitalWrite(d, n[i][3]);
+  digitalWrite(e, n[i][4]);
+  digitalWrite(f, n[i][5]);
+  digitalWrite(g, n[i][6]);
 }
